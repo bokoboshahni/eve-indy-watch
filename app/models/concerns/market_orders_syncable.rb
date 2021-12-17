0670c74192ffcc1dcf16b986fe08d557f6e2e46f
Bokobo Shahni <shahni@bokobo.space>
2021-12-17 14:49:26 -0500

    fix: class name for market order batches

--- app/models/concerns/market_orders_syncable.rb
+++ app/models/concerns/market_orders_syncable.rb
@@ -2,7 +2,7 @@ module MarketOrdersSyncable
   extend ActiveSupport::Concern
 
   included do
-    has_many :market_order_batches, as: :location
+    has_many :market_order_batches, 'MarketOrder::Batch', as: :location
   end
 
   def esi_market_orders_expired?
