05e933eec58ae8d84c4999c54c979438289e0e57
Bokobo Shahni <shahni@bokobo.space>
2021-12-17 13:05:01 -0500

    fix: remove old association

--- app/models/market.rb
+++ app/models/market.rb
@@ -30,7 +30,6 @@ class Market < ApplicationRecord
   has_many :alliances_as_main_market, class_name: 'Alliance', inverse_of: :main_market
   has_many :market_fitting_snapshots, inverse_of: :market, dependent: :destroy
   has_many :market_locations, inverse_of: :market, dependent: :destroy
-  has_many :market_type_stats, inverse_of: :market, dependent: :destroy
   has_many :orders, class_name: 'MarketOrder', through: :market_locations
   has_many :stations, through: :market_locations, source: :location, source_type: 'Station'
   has_many :structures, through: :market_locations, source: :location, source_type: 'Structure'
