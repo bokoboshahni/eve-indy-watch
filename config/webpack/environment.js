e18843d4883c5c98722a5271262c00b2aa9daea2
Bokobo Shahni <shahni@bokobo.space>
2021-12-20 18:45:44 -0500

    fix: turn off source maps

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,10 @@ const {environment} = require('@rails/webpacker')
 
 // Get the actual sass-loader config
 const sassLoader = environment.loaders.get('sass')
-const sassLoaderConfig = sassLoader.use.find(({ loader }) => loader === 'css-loader').options.sourceMap = false;
+sassLoader.use.find(({ loader }) => loader === 'css-loader').options.sourceMap = false;
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+  return element.loader == 'sass-loader'
+})
 
 // Use Dart-implementation of Sass (default is node-sass)
 const options = sassLoaderConfig.options
