70a6aadafb948a4ba8e9e676b049f8b9a548c9de
Bokobo Shahni <shahni@bokobo.space>
2021-12-20 19:44:02 -0500

    fix: csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ if Rails.env.production?
     policy.font_src    :self, :https, :data, ENV['RAILS_ASSET_HOST']
     policy.img_src     :self, :https, :data, ENV['RAILS_ASSET_HOST']
     policy.object_src  :none
-    policy.script_src  :self, :https, :unsafe_inline, 'https://js-agent.newrelic.com', 'https://bam.nr-data.net', 'https://bam-cell.nr-data.net', ENV['RAILS_ASSET_HOST']
+    policy.script_src  :self, :unsafe_inline, 'https://js-agent.newrelic.com', 'https://bam.nr-data.net', 'https://bam-cell.nr-data.net', ENV['RAILS_ASSET_HOST']
     policy.style_src   :self, :https, ENV['RAILS_ASSET_HOST']
 
     # Specify URI for violation reports
