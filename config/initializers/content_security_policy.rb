b8ff8aa13dfabc3cfac6b4203eaf818cfb2a9134
Bokobo Shahni <shahni@bokobo.space>
2021-12-20 21:23:37 -0500

    fix: csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
-# Be sure to restart your server when you modify this file.
-
-# Define an application-wide content security policy
-# For further information see the following documentation
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 if Rails.env.production?
   Rails.application.config.content_security_policy do |policy|
@@ -19,13 +14,6 @@ if Rails.env.production?
   end
 
   # If you are using UJS then enable automatic nonce generation
-  Rails.application.config.content_security_policy_nonce_generator = ->(r) { Digest::SHA1.hexdigest(r.session.id.to_s) }
+  Rails.application.config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
 end
-
-# Set the nonce only to specific directives
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
-
-# Report CSP violations to a specified URI
-# For further information see the following documentation:
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-# Rails.application.config.content_security_policy_report_only = true
