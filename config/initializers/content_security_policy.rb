bf5e18d37c6c849b26f0969d6c4c84cce8edc9a9
Bokobo Shahni <shahni@bokobo.space>
2021-12-21 10:06:27 -0500

    chore: enable asset host and csp for staging

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
-  Rails.application.config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data, ENV['RAILS_ASSET_HOST']
-    policy.img_src     :self, :https, :data, ENV['RAILS_ASSET_HOST']
-    policy.object_src  :none
-    policy.script_src  :self, :https, :unsafe_inline, 'https://js-agent.newrelic.com', 'https://bam.nr-data.net', 'https://bam-cell.nr-data.net', ENV['RAILS_ASSET_HOST']
-    policy.style_src   :self, :https, :unsafe_inline, ENV['RAILS_ASSET_HOST']
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data, ENV['RAILS_ASSET_HOST']
+  policy.img_src     :self, :https, :data, ENV['RAILS_ASSET_HOST']
+  policy.object_src  :none
+  policy.script_src  :self, :https, :unsafe_inline, 'https://js-agent.newrelic.com', 'https://bam.nr-data.net', 'https://bam-cell.nr-data.net', ENV['RAILS_ASSET_HOST']
+  policy.style_src   :self, :https, :unsafe_inline, ENV['RAILS_ASSET_HOST']
 
-    # Specify URI for violation reports
-    # policy.report_uri "/csp-violation-report-endpoint"
-  end
-
-  # If you are using UJS then enable automatic nonce generation
-  Rails.application.config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
 end
+
+# If you are using UJS then enable automatic nonce generation
+Rails.application.config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
