9aba2fcf61ecb2c587e7b27efb5ef730bbc57f3d
Bokobo Shahni <shahni@bokobo.space>
2021-12-20 20:49:36 -0500

    fix: csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,10 +17,10 @@ if Rails.env.production?
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
-end
 
-# If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+  # If you are using UJS then enable automatic nonce generation
+  Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+end
 
 # Set the nonce only to specific directives
 # Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
