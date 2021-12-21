c64e827b291af4ec2675355391588b199a692bd3
Bokobo Shahni <shahni@bokobo.space>
2021-12-21 11:09:27 -0500

    chore: allow rack-mini-profiler in staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,3 +100,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
+
+Rack::MiniProfiler.config.authorization_mode = :allow_authorized
