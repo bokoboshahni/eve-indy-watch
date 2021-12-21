ab9f518ee3d8b812973ce1a97d4f1084a07ac2eb
Bokobo Shahni <shahni@bokobo.space>
2021-12-20 19:37:37 -0500

    chore: configurable asset host

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,17 +5,19 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+if Rails.env.production?
+  Rails.application.config.content_security_policy do |policy|
+    policy.default_src :self, :https, ENV['RAILS_ASSET_HOST']
+    policy.font_src    :self, :https, :data, ENV['RAILS_ASSET_HOST']
+    policy.img_src     :self, :https, :data, ENV['RAILS_ASSET_HOST']
+    policy.object_src  :none
+    policy.script_src  :self, :https, :unsafe_inline, 'https://js-agent.newrelic.com', 'https://bam.nr-data.net', 'https://bam-cell.nr-data.net', ENV['RAILS_ASSET_HOST']
+    policy.style_src   :self, :https, ENV['RAILS_ASSET_HOST']
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+    # Specify URI for violation reports
+    # policy.report_uri "/csp-violation-report-endpoint"
+  end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
