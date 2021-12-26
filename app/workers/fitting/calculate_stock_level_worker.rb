ae3e533ae12270a0dde83004652e1c50f73dadd5
Bokobo Shahni <shahni@bokobo.space>
2021-12-26 09:45:53 -0500

    fix: parse time strings

--- app/workers/fitting/calculate_stock_level_worker.rb
+++ app/workers/fitting/calculate_stock_level_worker.rb
@@ -1,7 +1,10 @@
 class Fitting < ApplicationRecord
   class CalculateStockLevelWorker < ApplicationWorker
     def perform(fitting_id, market_id, market_time, time, interval = nil)
-      Fitting::CalculateStockLevel.call(fitting_id, market_id, market_time, time, interval)
+      Fitting::CalculateStockLevel.call(
+        fitting_id, market_id,
+        DateTime.parse(market_time), DateTime.parse(time), interval
+      )
     end
   end
 end
