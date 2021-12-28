4e65b2c5dca18570118d27bc310baa6f2270dbb6
Bokobo Shahni <shahni@bokobo.space>
2021-12-28 16:00:30 -0500

    refactor: move stylesheet import down

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,11 @@ const images = require.context('../images', true)
 import "@hotwired/turbo-rails"
 
 import LocalTime from 'local-time'
-import "stylesheets/application.scss"
 
 import 'chartkick/chart.js'
 
 LocalTime.start()
 
 import '../controllers'
+
+import "stylesheets/application.scss"
