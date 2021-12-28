62037b3ffea5270b08467426fe9cc61092a58812
Bokobo Shahni <shahni@bokobo.space>
2021-12-28 15:55:56 -0500

    refactor: type icon component

---
+++ app/components/type_icon_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class TypeIconComponent < ApplicationComponent
+  attr_reader :type, :classes
+
+  def initialize(type:, classes: nil)
+    @type = type
+    @classes = classes
+  end
+
+  def call
+    image_tag icon_url, class: classes
+  end
+
+  def icon_url
+    return asset_pack_path('media/images/blueprint.png') if type.blueprint?
+
+    type.icon_url
+  end
+end
