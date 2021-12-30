7d53108b89df77c3436a906d23297dcb2f0e0347
Bokobo Shahni <shahni@bokobo.space>
2021-12-30 18:57:15 -0500

    chore: remove old charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,6 @@ const images = require.context('../images', true)
 import "@hotwired/turbo-rails"
 
 import LocalTime from 'local-time'
-
-import 'chartkick/chart.js'
-
 LocalTime.start()
 
 import '../controllers'
