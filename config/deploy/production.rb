934c472e485a3fbb7022f95211d49477a3071b53
Bokobo Shahni <shahni@bokobo.space>
2022-01-08 10:28:01 -0500

    chore: use 12 workers in staging and production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,5 +7,5 @@ role :web, ENV.fetch('DEPLOY_PRODUCTION_ROLES_WEB', '').split(',')
 role :db, ENV.fetch('DEPLOY_PRODUCTION_ROLES_DB', '').split(',')
 role :worker, ENV.fetch('DEPLOY_PRODUCTION_ROLES_WORKER', '').split(',')
 
-set :sidekiq_processes, ENV.fetch('DEPLOY_PRODUCTION_SIDEKIQ_PROCESSES', 6).to_i
-set :sidekiq_concurrency, ENV.fetch('DEPLOY_PRODUCTION_SIDEKIQ_CONCURRENCY', 25).to_i
+set :sidekiq_processes, ENV.fetch('DEPLOY_PRODUCTION_SIDEKIQ_PROCESSES', 12).to_i
+set :sidekiq_concurrency, ENV.fetch('DEPLOY_PRODUCTION_SIDEKIQ_CONCURRENCY', 10).to_i
