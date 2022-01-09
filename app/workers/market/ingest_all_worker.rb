6567f8af10470e2920d138b7cb9df62fea043f97
Bokobo Shahni <shahni@bokobo.space>
2022-01-09 12:08:25 -0500

    fix: ensure only active markets are ingested

--- app/workers/market/ingest_all_worker.rb
+++ app/workers/market/ingest_all_worker.rb
@@ -3,7 +3,10 @@ class Market < ApplicationRecord
     sidekiq_options retries: 3, lock: :until_executed
 
     def perform
-      args = Market.where.not(source_location_id: nil).distinct(:source_location_id).pluck(:source_location_id).map { |id| [id] }
+      args = Market.active.where.not(source_location_id: nil)
+                   .distinct(:source_location_id)
+                   .pluck(:source_location_id)
+                   .map { |id| [id] }
       Location::SnapshotOrdersFromESIWorker.perform_bulk(args)
     end
   end
