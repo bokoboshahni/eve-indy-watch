706f5ea65bcff717f88c2e209ce19f358d7a5cd1
Bokobo Shahni <shahni@bokobo.space>
2022-01-11 20:08:15 -0500

    debug: log errors for regional market ingest

--- app/workers/market/calculate_type_statistics_worker.rb
+++ app/workers/market/calculate_type_statistics_worker.rb
@@ -3,7 +3,12 @@ class Market < ApplicationRecord
     def perform(market_id, type_ids, time, force = false)
       time = time.to_s.to_datetime
       type_ids.each do |type_id|
-        Market::CalculateTypeStatistics.call(market_id, type_id, time, force: force)
+        begin
+          Market::CalculateTypeStatistics.call(market_id, type_id, time, force: force)
+        rescue Redis::CommandError => e
+          raise if e.message !~ /mget/
+          error("No order sets found for #{type_id} for #{market_id} at #{time.to_s(:db)}")
+        end
       end
 
       Market::PersistTypeStatisticsWorker.perform_async(market_id, type_ids, time.to_s(:number).to_i)
