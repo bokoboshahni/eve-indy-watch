b6d6b41dafa6e4ac10378127ed3a38609dd6af3a
Bokobo Shahni <shahni@bokobo.space>
2022-01-12 20:28:29 -0500

    fix: class not module

--- lib/capistrano/prometheus/systemd.rb
+++ lib/capistrano/prometheus/systemd.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Capistrano
-  module Prometheus
+  class Prometheus < Capistrano::Plugin
     class Systemd < Capistrano::Plugin
       include PrometheusCommon
 
