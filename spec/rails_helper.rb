4c7dcd6c935e11bd48557fb1a8fe2b6ef21ac738
Bokobo Shahni <shahni@bokobo.space>
2022-01-30 16:23:18 +0000

    test: request spec for homepage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
 
+  config.include SSOHelpers, type: :request
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
 
